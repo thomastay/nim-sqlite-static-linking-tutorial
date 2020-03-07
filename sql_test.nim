e24f2f61dc8bfb7a01ce2005ccbf4971c8a1b374
ttay <ttay@umich.edu>
2020-03-06 21:09:15 -0500

    db

--- sql_test.nim
+++ sql_test.nim
@@ -2,7 +2,7 @@ import db_sqlite
 
 # user, password, database name can be empty.
 # These params are not used on db_sqlite module.
-let db = open("test_db.etilqs", "", "", "")
+let db = open("test.db", "", "", "")
 db.exec(sql"""CREATE TABLE IF NOT EXISTS my_table (
                  id   INTEGER,
                  name VARCHAR(50) NOT NULL
